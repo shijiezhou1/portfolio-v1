cce94f93a3bb1df33a6d9c7737e8c86c21b7ac62
shijiezhou <cs5129606@gmail.com>
2018-05-13 10:31:30 -0600

    updated

---
+++ @@@/CRUD Files/index.php
@@ -0,0 +1,49 @@
+<?php
+//including the database connection file
+include_once("db.php");
+ 
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+ 
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Homepage</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+ 
+<body class="container">
+    
+    <a href="add.html">Add New Data</a><br/><br/>
+ 
+    <table width='532' border=0>
+        <tr bgcolor='#CCCCCC'>
+            <td width="102">Name</td>
+            <td width="102">Age</td>
+            <td width="102">Email</td>
+            <td width="103">Update</td>
+            <td width="101">Extra</td>
+
+        </tr>
+        <?php 
+        //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+        while($res = mysqli_fetch_array($result)) {         
+            echo "<tr>";
+            echo "<td>".$res['name']."</td>";
+            echo "<td>".$res['age']."</td>";
+            echo "<td>".$res['email']."</td>";    
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+        }
+        ?>
+    </table>
+</body>
+</html>

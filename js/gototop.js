6635d64a784216b8321aa50cb9641f2665643583
shijiezhou <cs5129606@gmail.com>
2018-05-20 09:42:16 -0600

    updated the go to top menus
    s

---
+++ js/gototop.js
@@ -0,0 +1,15 @@
+//code is referece to the simple W3school code 
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+    document.getElementById("topBtn").style.display = "block";
+  }else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+  function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }

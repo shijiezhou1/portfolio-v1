3bc5d37579e10d68ea2a52b76b8ec5c67ca8f702
shijiezhou <cs5129606@gmail.com>
2018-06-09 08:28:49 -0600

    updated

--- test.php
+++ test.php
@@ -18,7 +18,7 @@
 // 目前时间
 //echo date('Y-m-d H:i:s'); // out:2018-05-26 23:04:00
 echo '<br />';
- 
+
 // 动态生成时间 范围在今天下午六点到晚上零点前的任意时间
 // $datetime = date('Y-m-d').' '.rand('18,23').':'.rand('0,59').':'.rand('0,59');  // 2012-05-27 19:20:00
 
@@ -26,13 +26,13 @@ $datetime = date('2018-05-26 23:05:00');
 
 // 算得时间戳
 $a = strtotime($datetime);
- 
+
 // 算得时间差
-$reduce = $a-time();
- 
+$reduce = $a - time();
+
 // sleep等待
 sleep($reduce);
- 
+
 // 执行到时间后执行的代码块
 print_r(date('Y-m-d H:i:s'));  // out:2012-05-27 19:20:00
 

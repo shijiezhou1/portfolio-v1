726ef48eb1c01a99e2276beb8462ae2e88caac9b
Shijie Zhou <cs5129606@gmail.com>
2018-08-20 22:45:42 -0600

    updated some information

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * This file is used for the personal
+ *
+ * @summary Write a description for the file
+ * @author Shijie Zhou
+ *
+ * Created at     : 2018-08-20 07:11:18 
+ * Last modified  : 2018-08-20 07:11:51
+ */
+
 var firebaseinput = document.getElementById("firebaseinput");
 var firebasebutton = document.getElementById("firebasebutton");
 var firebasebutton2 = document.getElementById("firebasebutton2");
@@ -5,7 +15,6 @@ var firebasebutton2 = document.getElementById("firebasebutton2");
 function setValue(){
   var firebaseRef = firebase.database().ref();  
   var textFirst = firebaseinput.value;
-
   firebaseRef.push("ok").set(textFirst);
 }
 

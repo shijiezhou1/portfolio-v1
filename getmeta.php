85c1a6568c440c672dcc0636219a5be57fe3b51e
Shijie Zhou <cs5129606@gmail.com>
2018-10-14 17:50:34 -0600

    Update getmeta.php

--- getmeta.php
+++ getmeta.php
@@ -1,9 +1,6 @@
 <h2>
 <?php
-// Assuming the above tags are at www.example.com
-$tags = get_meta_tags('http://www.spotlighthometours.com/tours/video-player-j.php?type=video&autoPlay=true&id=3054590');
 //$tags2 = get_meta_tags('https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/build-a-technical-documentation-page');
-
 // Notice how the keys are all lowercase now, and
 // how . was replaced by _ in the key.
 print_r($tags);

42126a961e556c474cc6509d27883ad14222e224
Shijie Zhou <cs5129606@gmail.com>
2019-04-16 21:42:57 -0600

    update test index

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('works!');
+function SuperType() {
+    this.property = true;
+}
+
+function SubType() {
+    this.subproperty = false;
+}
+
+SuperType.prototype.getSuperValue = function() {
+    return this.property;
+}
+
+
+// 这里是关键，创建SuperType的实例，并将该实例赋值给SubType.prototype
+SubType.prototype = new SuperType(); 
+SubType.prototype.getSubValue = function() {
+    return this.subproperty;
+}
+
+var instance = new SubType();
+console.log(instance); // true

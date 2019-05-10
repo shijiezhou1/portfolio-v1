c8f09436d40deb8e15cbc02838b9ddc00d2546df
Shijie Zhou <cs5129606@gmail.com>
2019-05-10 07:19:24 -0600

    save unknow error

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function SuperType() {
-    this.property = true;
-}
+// function SuperType() {
+//     this.property = true;
+// }
 
-function SubType() {
-    this.subproperty = false;
-}
+// function SubType() {
+//     this.subproperty = false;
+// }
 
-SuperType.prototype.getSuperValue = function() {
-    return this.property;
-}
+// SuperType.prototype.getSuperValue = function() {
+//     return this.property;
+// }
 
 
-// 这里是关键，创建SuperType的实例，并将该实例赋值给SubType.prototype
-SubType.prototype = new SuperType(); 
-SubType.prototype.getSubValue = function() {
-    return this.subproperty;
-}
+// // 这里是关键，创建SuperType的实例，并将该实例赋值给SubType.prototype
+// SubType.prototype = new SuperType(); 
+// SubType.prototype.getSubValue = function() {
+//     return this.subproperty;
+// }
 
-var instance = new SubType();
-console.log(instance); // true
+// var instance = new SubType();
+// console.log(instance); // true

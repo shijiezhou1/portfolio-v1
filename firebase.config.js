1d25772ea1e636b48c39cc1d541c3b3b8d9d9fff
Shijie Zhou <cs5129606@gmail.com>
2019-05-19 23:07:20 -0600

    Adding useful tag and remove some icon

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCsjNe80J0i2ZlR5PKJW9EBQMkHZjZM3mQ",
+    authDomain: "website-7b86a.firebaseapp.com",
+    databaseURL: "https://website-7b86a.firebaseio.com",
+    projectId: "website-7b86a",
+    storageBucket: "website-7b86a.appspot.com",
+    messagingSenderId: "577143481839"
+};
+//# sourceMappingURL=firebase.config.js.map
